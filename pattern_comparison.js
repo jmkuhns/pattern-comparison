d709bf9b2136858689ac21617cb459d5cd8ab4e5
jmkuhns <jmkuhns@uncg.edu>
2020-08-23 15:28:56 -0400

    no_task_yet_working_on_patterns

--- pattern_comparison.js
+++ pattern_comparison.js
@@ -1,1 +1,5 @@
 // pattern_comparison.js
+// Salthouse and Babcock: letter comparison is randomly chosen consonants of length 3, 6, or 9
+// Paper version had two separately timed versions with 32 pairs per pages
+// half are same, half are different
+// Difference is in 1 LD for a string
