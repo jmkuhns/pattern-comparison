d2280e079709dabfd6e277ad68be563344e9192c
jmkuhns <jmkuhns@uncg.edu>
2020-09-08 17:29:59 -0400

    trying out the practice test now and setting up qualtrics

--- experiment.js
+++ experiment.js
@@ -1,97 +1,150 @@
-// Use JSDELIVR to get the files from a GitHub repository
-// https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/<specific folder or file>
-//var repo_site = "https://cdn.jsdelivr.net/gh/kywch/jsPsych-in-Qualtrics/flanker/ArrowFlankerBitmaps";
-//var repo_site = "https://jmkuhns.github.io/flanker/";
 var timeline = [];
-var patterns = "https://jmkuhns.github.io/pattern-comparison/";
+var patterns = "https://jmkuhns.github.io/pattern-comparison/patterns/";
+var patterns_prac = "https://jmkuhns.github.io/pattern-comparison/patterns_practice/";
 
 /*set up welcome block*/
 var welcome = {
   type: "html-keyboard-response",
-  stimulus: "Welcome to the experiment. Press any key to begin."
+  stimulus: "Welcome to the experiment. Press any key to begin.",
+  data:{
+    exp_stage: "instructions"
+  }
 };
 timeline.push(welcome);
 /*set up instructions block*/
 var instructions = {
   type: "html-keyboard-response",
   stimulus:
-  "<p> In this task, there will be a plus sign (+) in the center of the screen.</p>"+
-  "<p> It is very important that you try to keep your eyes on the plus sign throughout the experiment.</p>"+
-  "<p> Your goal in this experiment is to decide which direction an arrow is pointing. <br> The arrows will appear above the plus sign.  However, you should still try not to move your eyes from the plus sign.</p>"+
-  "<br><p>Please press any key to continue </p>",
-  post_trial_gap: 1000
+  '<p style:"font-size:30px">PATTERN COMPARISON</p>' +
+  '<br><br><p>In this experiment you will be asked to determine whether two patterns of lines are the same or different. If the two patterns are the SAME, press the LEFT ARROW KEY. If the two patterns are DIFFERENT, press the RIGHT ARROW KEY. Please try to work as rapidly as you can.</p>' +
+  '<br><p>You will a few practice trials with feedback before starting.</p>'+
+  '<br><p>Press any key to continue.</p>'+,
+  post_trial_gap: 1000,
+  data:{
+    exp_stage: "instructions"
+  }
 };
 var instructions2 = {
   type: "html-keyboard-response",
-  stimulus:
-   "<p>In this task, you will see five arrows on the screen, like the example below.</p>" +
-    "<img src='https://jmkuhns.github.io/flanker/ArrowFlankersBitmaps/L.png'></img>" +
-    "<p>Press the left arrow key if the middle arrow is pointing left. (<)</p>" +
-    "<p>Press the right arrow key if the middle arrow is pointing right. (>)</p>" +
-    "<br><p>Press any key to complete a practice block.</p>",
+  stimulus:'<p>As a reminder, if the two patterns are the SAME, press the LEFT ARROW KEY. If the two patterns are DIFFERENT, press the RIGHT ARROW KEY.</p>' +
+  '<br><p>Press any key to begin the practice trials.</p>',
   post_trial_gap: 1000
 };
 timeline.push(instructions, instructions2);
 
-// how many trials????????????????/
-/* experiment parameters */
-var reps_per_practice = 5; // 5 * 6 = 30
-var reps_per_trial_type = 40; // 40 * 6 = 240
+var prac_correct = [37, 39, 39];
+var practice_left = [
+  {stimulus: patterns_practice + "prac_1_1.png",
+  data: {stim: "prac_1_1.png", corr_resp: prac_correct[0], exp_stage: "practice", position: "left"}
+  },
+  {stimulus: patterns_practice + "prac_2_1.png",
+  data: {stim: "prac_2_1.png", corr_resp: prac_correct[1], exp_stage: "practice", position: "left"}
+  },
+  {stimulus: patterns_practice + "prac_3_1.png",
+  data: {stim: "prac_3_1.png", corr_resp: prac_correct[2], exp_stage: "practice", position: "left"}
+  }];
+var practice_right = [
+    {stimulus: patterns_practice + "prac_1_2.png",
+    data: {stim: "prac_1_2.png", corr_resp: prac_correct[0], exp_stage: "practice", position: "right"}
+    },
+    {stimulus: patterns_practice + "prac_2_2.png",
+    data: {stim: "prac_2_2.png", corr_resp: prac_correct[1], exp_stage: "practice", position: "right"}
+    },
+    {stimulus: patterns_practice + "prac_3_2.png",
+    data: {stim: "prac_3_2.png", corr_resp: prac_correct[2], exp_stage: "practice", position: "right"}
+    }];
+var num_practice_trials = 3;
+for (i = 0; i < num_practice_trials; i++){
 
-var fixation = {
-      type: 'html-keyboard-response',
-      stimulus: '<div style="font-size:40px;">+</div>',
-      choices: jsPsych.NO_KEYS,
-      trial_duration: function() {
-                    return Math.floor(Math.random() * 1500) + 500;
-                },
-      data: {test_part: 'fixation'}
+var practice_left_loop = practice_left[i];
+var practice_right_loop = practice_right[i];
+var prac_correct_loop = prac_correct[i];
+
+var practice_test = {
+      type: "image-keyboard-response",
+      choices: [37, 39],
+      stimulus: '<div class="row"><div class="column"><img src=' + practice_left_loop + ' style="width:50%"></div><div class="column"><img src=' + practice_right_loop + ' style="width:50%></div></div>"',
+      data: {
+        exp_stage: "pattern comp",
+        corr_resp: prac_correct_loop
+      },
+  		response_ends_trial: true,
+  		on_finish: function(data){
+  			if (data.key_press == data.corr_resp){
+  				data.accuracy = 1;
+  			}else {
+  					data.accuracy = 0;
+  				}
+        }
     };
+    timeline.push(practice_test);
+}
 
 /*defining stimuli*/
-var test_stimuli = [
-  {
-    stimulus: flanker_bitmaps + "RH.png",
-    data: { stim_type: 'congruent', direction: 'right'}
-  },
-  {
-    stimulus: flanker_bitmaps + "LH.png",
-    data: { stim_type: 'congruent', direction: 'left'}
-  },
-  {
-    stimulus: flanker_bitmaps + "LRL.png",
-    data: { stim_type: 'incongruent', direction: 'right'}
-  },
-  {
-    stimulus: flanker_bitmaps + "RLR.png",
-    data: { stim_type: 'incongruent', direction: 'left'}
-  },
-  {
-    stimulus: flanker_bitmaps + "dotL.png",
-    data: { stim_type: 'neutral', direction: 'left'}
-  },
-  {
-    stimulus: flanker_bitmaps + "dotR.png",
-    data: { stim_type: 'neutral', direction: 'right'}
-  }
-];
+// set up numbers to coordinate with the file names
+var nums_01 = ['01', '02', '03', '04', '05', '06', '07', '08', '09'];
+var nums_2 = [];
+for (i = 10; i < 31; i++){nums_2.push(i)}
+nums_01 = nums_01.concat(nums_2);
 
-var test = {
-  type: "image-keyboard-response",
-  choices: [37, 39],
-  trial_duration: 1500,
-  stimulus: jsPsych.timelineVariable('stimulus'),
-  data: jsPsych.timelineVariable('data'),
-  on_finish: function(data){
-    var correct = false;
-    if(data.direction == 'left' &&  data.key_press == 37 && data.rt > -1){
-      correct = true;
-    } else if(data.direction == 'right' && data.key_press == 39 && data.rt > -1){
-      correct = true;
-    }
-    data.correct = correct;
-  }
+
+var patterns_page_1_left = [];
+for (i = 0; i<30; i++){
+  patterns_page_1_left.push( "1_" + nums_01[i] + "_01")
 }
+
+var patterns_page_1_right = [];
+for (i = 0; i<30; i++){
+  patterns_page_1_right.push( "1_" + nums_01[i] + "_02")
+}
+
+var patterns_page_2_left = [];
+for (i = 0; i<30; i++){
+  patterns_page_2_left.push( "2_" + nums_01[i] + "_01")
+}
+
+var patterns_page_2_right = [];
+for (i = 0; i<30; i++){
+  patterns_page_2_right.push( "2_" + nums_01[i] + "_02")
+}
+// 37 = same; 39 = diff
+var p1_correct = [37, 39, 37, 39, 37, 39, 37, 39, 37, 39, 37, 39, 39, 39, 37, 39, 37, 39, 39, 37, 37, 37, 37, 37, 39, 39, 39, 37, 39, 37];
+
+var p2_correct = [39, 39, 37, 37, 37, 37, 39, 39, 37, 37, 39, 39, 37, 39, 39, 39, 37, 37, 37, 37, 37, 39, 39, 39, 37, 39, 39, 37, 37, 39];
+
+for (i = 0; i < 30; i ++){
+
+    var p1_left = [{
+      stimulus: patterns + patterns_page_1_left[i],
+      data: {stim: i, corr_resp: p1_correct[i], exp_stage: "pattern", position: "left"}
+    }];
+    var p1_right = {
+      stimulus: patterns + patterns_page_1_right[i],
+      data: {stim: i, corr_resp: p1_correct[i], exp_stage: "pattern", position: "right"}
+    }];
+  var cresp = p1_correct[i];
+  var pattern_name: patterns_page_1_left[i];
+  var test = {
+    type: "image-keyboard-response",
+    choices: [37, 39],
+    stimulus: '<div class="row"><div class="column"><img src=' + p1_left + ' style="width:50%"></div><div class="column"><img src=' + p1_right + ' style="width:50%></div></div>"',
+    data: {
+      exp_stage: "pattern comp",
+      corr_resp: cresp,
+      stim: pattern_name
+    },
+		response_ends_trial: true,
+		on_finish: function(data){
+			if (data.key_press == data.corr_resp){
+				data.accuracy = 1;
+			}else {
+					data.accuracy = 0;
+				}
+      }
+  }
+  timeline.push(test);
+}
+
 
 var test_practice = {
   timeline: [fixation, test],
