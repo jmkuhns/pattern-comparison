326b3f25e0dc22e4d72de67d8640276c048a3ebc
jmkuhns <jmkuhns@uncg.edu>
2020-09-15 16:41:14 -0400

    alt timeout

--- experiment.js
+++ experiment.js
@@ -605,7 +605,7 @@ var alt_test_trials = {
     post_trial_gap: 250,
     on_start:function(){
       var trial_timeout = setTimeout(function () {
-            jsPsych.endCurrentTimeline();
+            jsPsych.endExperiment("gotcha!~");
         }, 3000);
       },
     // trial_duration: current_timer,
