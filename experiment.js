173b15e126647e6fdc75293678edbf5f1db414d9
jmkuhns <jmkuhns@uncg.edu>
2020-09-15 16:03:39 -0400

    practice_again_1

--- experiment.js
+++ experiment.js
@@ -536,15 +536,15 @@ var prac_node = {
     {stimulus_1: patterns_practice + "prac_1_1.png",
       stimulus_2: patterns_practice + "prac_1_2.png",
         data: {corr_resp: 37, exp_stage: "practice" ,
-      stim: patterns_practice + "prac_1_1.png"}},
+      stim:  "prac_1_1.png"}},
     {stimulus_1: patterns_practice + "prac_2_1.png",
       stimulus_2: patterns_practice + "prac_2_2.png",
         data: {corr_resp: 39, exp_stage: "practice",
-      stim: patterns_practice + "prac_2_1.png"}},
+      stim:  "prac_2_1.png"}},
     {stimulus_1: patterns_practice + "prac_3_1.png",
       stimulus_2: patterns_practice + "prac_3_2.png",
         data: {corr_resp: 39, exp_stage: "practice",
-      stim: patterns_practice + "prac_3_1.png"}}
+      stim:  "prac_3_1.png"}}
   ]
 };
 
